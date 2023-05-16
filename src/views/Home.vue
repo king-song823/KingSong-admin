63c309b90e50e98f55930c26cc9918f597d3e433
ink-song <229135518@qq.com>
2023-05-16 22:35:55 +0800

    feat: prettier 代码格式美化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: ink-song 229135518@qq.com
+ * @Date: 2023-05-16 22:02:24
+ * @LastEditors: ink-song 229135518@qq.com
+ * @LastEditTime: 2023-05-16 22:26:38
+ * @FilePath: /imooc-admin/src/views/Home.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
@@ -7,12 +15,12 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld,
-  },
-};
+    HelloWorld
+  }
+}
 </script>
