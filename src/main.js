059d7fd9decd482c6a1f254843b97417861f867c
ink-song <229135518@qq.com>
2023-05-17 23:49:12 +0800

    test: init

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@
  * @Author: ink-song 229135518@qq.com
  * @Date: 2023-05-16 22:02:24
  * @LastEditors: ink-song 229135518@qq.com
- * @LastEditTime: 2023-05-17 23:40:49
+ * @LastEditTime: 2023-05-17 23:48:34
  * @FilePath: /imooc-admin/src/main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from "./router"
+import router from './router'
 createApp(App).use(router).mount('#app')
