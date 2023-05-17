e0888c407e134db4d855d182babc38fb086a0a8d
ink-song <229135518@qq.com>
2023-05-17 23:23:59 +0800

    feat: 强制校验提交信息

---
+++ commitlint.config.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: ink-song 229135518@qq.com
+ * @Date: 2023-05-16 23:21:17
+ * @LastEditors: ink-song 229135518@qq.com
+ * @LastEditTime: 2023-05-16 23:21:40
+ * @FilePath: /imooc-admin/commitlint.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+  // 继承的规则
+  extends: ['@commitlint/config-conventional'],
+  // 定义规则类型
+  rules: {
+    // type 类型定义，表示 git 提交的 type 必须在以下类型范围内
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // 新功能 feature
+        'fix', // 修复 bug
+        'docs', // 文档注释
+        'style', // 代码格式(不影响代码运行的变动)
+        'refactor', // 重构(既不增加新功能，也不是修复bug)
+        'perf', // 性能优化
+        'test', // 增加测试
+        'chore', // 构建过程或辅助工具的变动
+        'revert', // 回退
+        'build' // 打包
+      ]
+    ],
+    // subject 大小写不做校验
+    'subject-case': [0]
+  }
+}
