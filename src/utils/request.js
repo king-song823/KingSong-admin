de7f04243c4c3a1e1a9c4efca754485fdbaab348
ink-song <229135518@qq.com>
2023-06-05 23:28:44 +0800

    feat: update

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Ice songbing940823@gmail.com
  * @Date: 2023-05-30 16:03:21
- * @LastEditors: Ice songbing940823@gmail.com
- * @LastEditTime: 2023-06-05 18:00:08
+ * @LastEditors: ink-song 229135518@qq.com
+ * @LastEditTime: 2023-06-05 22:42:35
  * @FilePath: /imooc-admin/src/utils/request.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,10 +16,9 @@ const service = axios.create({
 })
 
 service.interceptors.request.use((config) => {
-  // const { icode, time } = getTestICode()
-  const { time } = getTestICode()
+  const { icode, time } = getTestICode()
   config.headers.codeType = time
-  // config.headers.icode = icode
+  config.headers.icode = icode
   return config
 })
 
