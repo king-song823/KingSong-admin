c5844d8faf5cfe38f71fef731bc64c36f314eebb
ink-song <229135518@qq.com>
2023-06-26 08:37:06 +0800

    feat: 增加路由和左侧导航

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,145 @@
 /*
  * @Author: ink-song 229135518@qq.com
  * @Date: 2023-05-16 22:02:24
- * @LastEditors: Ice songbing940823@gmail.com
- * @LastEditTime: 2023-06-06 13:58:04
+ * @LastEditors: ink-song 229135518@qq.com
+ * @LastEditTime: 2023-06-25 17:24:37
  * @FilePath: /imooc-admin/src/router/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { createRouter, createWebHistory } from 'vue-router'
-const pucliRoutes = [
+
+const publicRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index')
+  },
   {
     path: '/',
-    component: () => import('../layout/index.vue')
+    // 注意：带有路径“/”的记录中的组件“默认”是一个不返回 Promise 的函数
+    component: () => import('@/layout/index'),
+
+    redirect: '/profile',
+    children: [
+      {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('@/views/profile/index'),
+        meta: {
+          title: 'profile',
+          icon: 'el-icon-user'
+        }
+      },
+      {
+        path: '/404',
+        name: '404',
+        component: () => import('@/views/error-page/404')
+      },
+      {
+        path: '/401',
+        name: '401',
+        component: () => import('@/views/error-page/401')
+      }
+    ]
   }
 ]
 
+const privateRoutes = [
+  {
+    path: '/user',
+    component: () => import('@/layout/index'),
+    redirect: '/user/manage',
+    meta: {
+      title: 'user',
+      icon: 'personnel'
+    },
+    children: [
+      {
+        path: '/user/manage',
+        component: () => import('@/views/user-manage/index'),
+        meta: {
+          title: 'userManage',
+          icon: 'personnel-manage'
+        }
+      },
+      {
+        path: '/user/role',
+        component: () => import('@/views/role-list/index'),
+        meta: {
+          title: 'roleList',
+          icon: 'role'
+        }
+      },
+      {
+        path: '/user/permission',
+        component: () => import('@/views/permission-list/index'),
+        meta: {
+          title: 'permissionList',
+          icon: 'permission'
+        }
+      },
+      {
+        path: '/user/info/:id',
+        name: 'userInfo',
+        component: () => import('@/views/user-info/index'),
+        meta: {
+          title: 'userInfo'
+        }
+      },
+      {
+        path: '/user/import',
+        name: 'import',
+        component: () => import('@/views/import/index'),
+        meta: {
+          title: 'excelImport'
+        }
+      }
+    ]
+  },
+  {
+    path: '/article',
+    component: () => import('@/layout/index'),
+    redirect: '/article/ranking',
+    meta: {
+      title: 'article',
+      icon: 'article'
+    },
+    children: [
+      {
+        path: '/article/ranking',
+        component: () => import('@/views/article-ranking/index'),
+        meta: {
+          title: 'articleRanking',
+          icon: 'article-ranking'
+        }
+      },
+      {
+        path: '/article/:id',
+        component: () => import('@/views/article-detail/index'),
+        meta: {
+          title: 'articleDetail'
+        }
+      },
+      {
+        path: '/article/create',
+        component: () => import('@/views/article-create/index'),
+        meta: {
+          title: 'articleCreate',
+          icon: 'article-create'
+        }
+      },
+      {
+        path: '/article/editor/:id',
+        component: () => import('@/views/article-create/index'),
+        meta: {
+          title: 'articleEditor'
+        }
+      }
+    ]
+  }
+]
 const routes = [
-  ...pucliRoutes,
+  ...privateRoutes,
+  ...publicRoutes,
   {
     path: '/login',
     name: 'Login',
