292b71a5e8149484e25d30ccfb417ebec9e366c8
ice-7777777 <15519586771@163.com>
2023-06-26 18:28:35 +0800

    style: 导航样式优化

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -2,13 +2,19 @@
  * @Author: ice-7777777 15519586771@163.com
  * @Date: 2023-06-26 09:36:08
  * @LastEditors: ice-7777777 15519586771@163.com
- * @LastEditTime: 2023-06-26 17:26:07
+ * @LastEditTime: 2023-06-26 18:26:31
  * @FilePath: /imooc-admin/src/layout/components/Sidebar/SidebarMenu.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <!-- 一级 menu 菜单 -->
-  <el-menu>
+  <el-menu
+    :default-active="activeMenu"
+    :background-color="variables.menuBg"
+    :text-color="variables.menuText"
+    :active-text-color="variables.menuActiveText"
+    :unique-opened="true"
+  >
     <sidebar-item
       v-for="item in routes"
       :key="item.path"
@@ -37,16 +43,20 @@
   </el-menu> -->
 </template>
 <script setup>
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 import SidebarItem from '../SidebarItem.vue'
 const router = useRouter()
-console.log(router.getRoutes())
+import variables from '@/styles/variables.scss'
+console.log('variables', variables)
 import { computed } from 'vue'
 import { filterRouters, generateMenus } from '@/utils/route'
-
+const route = useRoute()
 const routes = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   return generateMenus(filterRoutes)
 })
-console.log(JSON.stringify(routes.value))
+const activeMenu = computed(() => {
+  const { path } = route
+  return path
+})
 </script>
